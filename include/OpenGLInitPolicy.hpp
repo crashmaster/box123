461cce87bdfb77f9cddc6dd15b7127c7e97eebdd
Johnny Crash <cannon.imus@gmail.com>
2013-11-01 17:49:50 +0100

    Removed residues.

--- include/OpenGLInitPolicy.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __OPEN_GL_INIT_POLICY_HPP__
-#define __OPEN_GL_INIT_POLICY_HPP__
-
-class OpenGLInitPolicy {
-  protected:
-    void init(const int width, const int height) const;
-};
-
-#endif
